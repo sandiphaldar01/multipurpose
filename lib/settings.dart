c8eac369bace92ac2a304877da762228c536909a
Sandip Haldar <haldarsandip49@gmail.com>
2026-08-21 10:25:59 +0530

    add text and svg icon in setting page

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Settings extends StatefulWidget {
   const new({super.key});
@@ -11,12 +12,44 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            'Settings'
-          )
-        ],
+      body: SafeArea(
+        child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              SizedBox(height: 30,),
+              Text(
+                'Settings',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 15,),
+              Stack(
+                alignment: AlignmentDirectional.bottomEnd,
+                children: [ 
+                  SvgPicture.asset(
+                  'assets/profile.svg',
+                  height: 100,
+                  width: 100,
+                  fit: BoxFit.cover,
+                 ),
+                Container(
+                  padding: EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    color: Color(0xff006FFD),
+                    shape: BoxShape.circle
+                  ),
+                  child: Icon(
+                    Icons.edit
+                  ),
+                ),
+               ]
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
